5df9fd55a7160cdaf5034ad82defcac62647d09b
Tomkettle1 <tomkettleone@gmail.com>
2020-12-08 14:28:22 +0000

    Tree & Forest Classes (#5)
    * created tree.cs
    
    * Renamed w/ capital
    
    * Renamed w/ capital
    
    * added ICuttable & Fir
    
    * Added ITapable, Spruce, Maple
    
    * delete (temp)
    
    * recreated tree
    
    * Created forest.cs

---
+++ Forest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForestSim
+{
+    class Forest
+    {
+        private Tree[,] _trees;
+        public Forest(int ForestWidth, int ForestHeight)
+        {
+            _trees = new Tree[ForestWidth, ForestHeight];
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForestSim
+{
+    interface ICuttable
+    {
+        bool Cut();
+    }
+    interface ITapable
+    {
+        bool Tap();
+    }
+    class Tree
+    {
+        protected int _age;
+        protected bool _diseased;
+    }
+    class Fir : Tree, ICuttable
+    {
+        public bool Cut()
+        {
+            return true;
+        }
+    }
+    class Spruce : Tree, ICuttable
+    {
+        public bool Cut()
+        {
+            return true;
+        }
+    }
+    class Maple : Tree, ICuttable, ITapable
+    {
+        public bool Cut()
+        {
+            return true;
+        }
+
+        public bool Tap()
+        {
+            return true;
+        }
+    }
+}
