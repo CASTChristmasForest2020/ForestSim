ef846faebe2161489932fe15e29ed2d3fa093fd5
Tomkettle <tomkettleone@gmail.com>
2020-12-09 09:06:15 +0000

    removed old test code

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ namespace ForestSim
     {
         static void Main(string[] args)
         {
-            Tree tree1 = new Tree(378);
-            Tree tree2 = new Tree(-38);
-            Console.WriteLine("{0}, {1}; {2}, {3};", tree1.Age_Days, tree1.Age_Years, tree2.Age_Days, tree2.Age_Years);
+            
         }
     }
 }
