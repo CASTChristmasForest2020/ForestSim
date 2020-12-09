79328f760f8e443c3ef09ff7ef37419ad43c46e5
Deepkumar Pawar <17U-DPawar@utccambridge.co.uk>
2020-12-09 10:03:27 +0000

    Created River class

---
+++ River.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForestSim
+{
+    class River
+    {
+        List<int[]> adjacentTreePositions = new List<int[]>();
+    }
+}
