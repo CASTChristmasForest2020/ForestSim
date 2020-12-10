f047b274d939f8e428feb4e621da565fe6ca63a3
Deepkumar Pawar <17U-DPawar@utccambridge.co.uk>
2020-12-10 15:52:07 +0000

    Finished river prototype and put tools to visulaize it

--- River.cs
+++ River.cs
@@ -4,8 +4,119 @@ using System.Text;
 
 namespace ForestSim
 {
-    class River
+    public class River
     {
-        List<int[]> adjacentTreePositions = new List<int[]>();
+        public List<Coordinate> RiverPositions;
+
+        public struct Coordinate
+        {
+            public double x;
+            public double y;
+
+            public Coordinate(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public River()
+        {
+            this.RiverPositions = new List<Coordinate>();
+        }
+
+        public void GenerateRiver()
+        {
+            Coordinate initalRiverPosition = GetStartingPosition(0, 100, 0, 100);
+
+            double riverAngle = GetStartingAngle("bottom");
+
+            Coordinate riverPosition = initalRiverPosition;
+
+            double increment = 1d;
+            double nextRiverAngleStandardDeviation = Math.PI / 6;
+
+            while (CheckIfInForestBounds(riverPosition, 0, 100, 0, 100))
+            {
+                RiverPositions.Add(riverPosition);
+
+                riverPosition = GetNextPosition(riverAngle, riverPosition, increment);
+                riverAngle = GetNextAngle(riverAngle, nextRiverAngleStandardDeviation);
+            }
+        }
+
+        public Coordinate GetStartingPosition(double lowerX, double upperX, double lowerY, double upperY)
+        {
+            double startX, startY;
+
+            startX = (double)GetRandomInBounds(lowerX, upperX);
+            startY = 0f;
+
+            Coordinate startingPosition = new Coordinate(startX, startY);
+
+            return startingPosition;
+        }
+
+        public double GetStartingAngle(string side)
+        {
+            double angle;
+
+            switch (side)
+            {
+                case "bottom":
+                    angle = GetRandomInBounds(0, Math.PI);
+                    break;
+                default:
+                    angle = 0f;
+                    break;
+            }
+
+            return Math.PI / 2;
+        }
+
+        public Coordinate GetNextPosition(double angle, Coordinate riverPosition, double increment)
+        {
+            double changeX, changeY;
+
+            changeX = increment * Math.Cos(angle);
+            changeY = increment * Math.Sin(angle);
+
+            Coordinate nextCoordinate = new Coordinate(riverPosition.x + changeX, riverPosition.y + changeY);
+
+            return nextCoordinate;
+        }
+
+        public double GetNextAngle(double previousAngle, double standardDeviation)
+        {
+            double angleChange = SampleGaussian(0, standardDeviation);
+
+            return previousAngle + angleChange;
+        }
+
+        public bool CheckIfInForestBounds(Coordinate riverPosition, double lowerX, double upperX, double lowerY, double upperY)
+        {
+            if (riverPosition.x < lowerX || riverPosition.x > upperX || riverPosition.y < lowerY || riverPosition.y > upperY)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public double GetRandomInBounds(double lowerBound, double upperBound)
+        {
+            Random rnd = new Random();
+            return (rnd.NextDouble() * (upperBound - lowerBound) + lowerBound);
+        }
+        public double SampleGaussian(double mean, double stddev)
+        {
+            Random rnd = new Random();
+
+            double x1 = 1 - rnd.NextDouble();
+            double x2 = 1 - rnd.NextDouble();
+
+            double y1 = Math.Sqrt(-2.0 * Math.Log(x1)) * Math.Cos(2.0 * Math.PI * x2);
+            return y1 * stddev + mean;
+        }
     }
 }
---
+++ RiverVisuals/Form1.cs
@@ -0,0 +1,54 @@
+﻿using ForestSim;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiverVisuals
+{
+    public partial class Form1 : Form
+    {
+        River river;
+        Point[] riverPoints;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            river = new River();
+
+            river.GenerateRiver();
+
+            riverPoints = new Point[river.RiverPositions.Count];
+
+            for (int i = 0; i < river.RiverPositions.Count; i++)
+            {
+
+                riverPoints[i].X = (int) river.RiverPositions.ToArray()[i].x * 10;
+                riverPoints[i].Y = (int) river.RiverPositions.ToArray()[i].y * 10;
+
+                if (i > 1)
+                {
+                    this.CreateGraphics().DrawLine(new Pen(Brushes.Black, 4), riverPoints[i-1], riverPoints[i]);
+                }
+                
+            }
+
+            
+        }
+    }
+}
